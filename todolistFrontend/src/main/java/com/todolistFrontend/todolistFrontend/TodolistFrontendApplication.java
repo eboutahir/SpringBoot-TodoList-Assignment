1e5c6aee20ee4c6d6d911854a14ca71eaaf18c9c
BABAHAJ Zakarya <z.babahaj@edu.umi.ac.ma>
2023-10-16 17:30:40 +0100

    Project Setup

---
+++ todolistFrontend/src/main/java/com/todolistFrontend/todolistFrontend/TodolistFrontendApplication.java
@@ -0,0 +1,12 @@
+package com.todolistFrontend.todolistFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
+public class TodolistFrontendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistFrontendApplication.class, args);
+    }
+
+}
---
+++ todolistFrontend/src/test/java/com/todolistFrontend/todolistFrontend/TodolistFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolistFrontend.todolistFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

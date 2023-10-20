b85a8ba1d886f87a26d78044083b85f170acbf57
Asmae <barhouasmae@gmail.com>
2023-10-20 20:23:04 +0200

    to do list

---
+++ src/test/java/asmae_B/asmae_A.java
@@ -0,0 +1,13 @@
+package asmae_B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class asmaeTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

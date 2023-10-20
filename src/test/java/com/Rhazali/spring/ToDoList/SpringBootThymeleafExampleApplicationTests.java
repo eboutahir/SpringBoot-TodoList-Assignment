3044041fad76245e1c2497556ff2e35c44dbac04
Your Name <rhazali@gmial.com>
2023-10-20 21:19:10 +0100

    RhazaliTodoList

---
+++ src/test/java/com/Rhazali/spring/ToDoList/SpringBootThymeleafExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rhazali.spring.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
